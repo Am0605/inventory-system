4d126c1ae1dd7d4102a3aa60524555b9b178a83c
Am0605 <adham8663@gmail.com>
2025-07-18 11:05:37 +0800

    fix: correct branding text in Welcome component

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -23,7 +23,7 @@ export default function Welcome() {
                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4" />
                                     </svg>
                                 </div>
-                                <span className="text-xl font-bold text-gray-900 dark:text-white">SmartInventory</span>
+                                <span className="text-xl font-bold text-gray-900 dark:text-white">Smart Inventory</span>
                             </div>
                         </div>
                         <div className="flex items-center space-x-4">
