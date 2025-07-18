d1cc1292878c1aee6a319cce30430f4c07124ea0
Am0605 <adham8663@gmail.com>
2025-07-18 12:10:21 +0800

    Implement code changes to enhance functionality and improve performance

--- resources/js/layouts/auth/auth-split-layout.tsx
+++ resources/js/layouts/auth/auth-split-layout.tsx
@@ -14,7 +14,7 @@ export default function AuthSplitLayout({ children, title, description }: PropsW
     return (
         <div className="relative grid h-dvh flex-col items-center justify-center px-8 sm:px-0 lg:max-w-none lg:grid-cols-2 lg:px-0">
             <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
-                <div className="absolute inset-0 bg-zinc-900" style={{ backgroundImage: 'url(/build/assets/image/factory.jpg)', backgroundSize: 'cover', backgroundPosition: 'left'}} />
+                <div className="absolute inset-0 bg-zinc-900" style={{ backgroundImage: 'url(/images/factory.jpg)', backgroundSize: 'cover', backgroundPosition: 'left'}} />
                 <div className="absolute inset-0 bg-gradient-to-t from-black/60 to-transparent" />
                 <Link href={route('home')} className="relative z-20 flex items-center text-lg font-medium">
                     <div className="w-8 h-8 bg-gradient-to-br from-blue-600 to-indigo-600 rounded-lg flex items-center justify-center">
